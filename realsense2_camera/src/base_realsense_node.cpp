8cfda7f024c1fa9550a443745d231c51a31a077c
arpright <arpit.chauhan@rightbot.in>
2023-06-21 18:18:21 +0530

    depth align filtering twice

--- realsense2_camera/src/base_realsense_node.cpp
+++ realsense2_camera/src/base_realsense_node.cpp
@@ -207,9 +207,9 @@ void BaseRealSenseNode::setupFilters()
     _filters.push_back(std::make_shared<NamedFilter>(std::make_shared<rs2::hole_filling_filter>(), _parameters, _logger));
     _filters.push_back(std::make_shared<NamedFilter>(std::make_shared<rs2::disparity_transform>(false), _parameters, _logger));
 
-    // _align_depth_raw_filter_initial = std::make_shared<AlignDepthFilter>(std::make_shared<rs2::align>(RS2_STREAM_COLOR), update_align_depth_func, _parameters, _logger);
-    // _raw_filters.push_back(_align_depth_raw_filter_initial);
-    _raw_filters.push_back(std::make_shared<NamedFilter>(std::make_shared<rs2::decimation_filter>(), _parameters, _logger));
+    _align_depth_raw_filter_initial = std::make_shared<AlignDepthFilter>(std::make_shared<rs2::align>(RS2_STREAM_COLOR), update_align_depth_func, _parameters, _logger);
+    _raw_filters.push_back(_align_depth_raw_filter_initial);
+    // _raw_filters.push_back(std::make_shared<NamedFilter>(std::make_shared<rs2::decimation_filter>(), _parameters, _logger));
     _raw_filters.push_back(std::make_shared<NamedFilter>(std::make_shared<rs2::hdr_merge>(), _parameters, _logger));
     _raw_filters.push_back(std::make_shared<NamedFilter>(std::make_shared<rs2::sequence_id_filter>(), _parameters, _logger));
     _raw_filters.push_back(std::make_shared<NamedFilter>(std::make_shared<rs2::disparity_transform>(), _parameters, _logger));
@@ -622,8 +622,8 @@ void BaseRealSenseNode::frame_callback(rs2::frame frame)
         for (int i = 0; i<static_cast<int>(_filters.size()); i++ )
         {   
             frameset = _filters[i]->Process(frameset);    
-            if (i == 0)
-                _raw_filters[i]->_is_enabled = false;
+            // if (i == 0)
+            //     _raw_filters[i]->_is_enabled = false;
             raw_frameset = _raw_filters[i]->Process(raw_frameset);
         }
 
