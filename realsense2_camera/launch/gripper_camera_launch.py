247432dec9795b90d45c2e781bc45e62ee8d8dd5
BhavyaBotta-Rightbot <bhavya.botta@rightbot.in>
2023-07-11 19:15:05 +0530

    run gripper cam pc with 720p resolution

--- realsense2_camera/launch/gripper_camera_launch.py
+++ realsense2_camera/launch/gripper_camera_launch.py
@@ -61,7 +61,7 @@ configurable_parameters = [{'name': 'camera_name',                  'default': '
 config = os.path.join(
         get_package_share_directory('realsense2_camera'),
         'config',
-        'gripper_camera_params_without_pc.yaml'
+        'gripper_camera_params_with_pc.yaml'
         )
 
 def declare_configurable_parameters(parameters):
